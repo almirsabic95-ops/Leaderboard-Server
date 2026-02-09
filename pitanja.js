cae3e9163cad0cd859dda20f22535ca33309865e
Almir Sabic <almirsabic95@example.com>
2026-02-09 21:02:21 +0100

    Dodane formalne kategorije i pitanja

--- pitanja.js
+++
@@ -1,9 +0,0 @@
-{
-  "acl-liste": [
-    { "pitanje": "Koji port koristi SSH?", "odgovor": "22" },
-    { "pitanje": "Koji je raspon za standardne ACL?", "odgovor": "1-99" }
-  ],
-  "python-coding": [
-    { "pitanje": "Koji tip podatka je True/False?", "odgovor": "boolean" }
-  ]
-}
